3062be2cc4d9850a387dcedd6ad5f3c2fac8e892
Cale Sennett <calesennett@gmail.com>
2015-08-08 00:32:09 -0400

    add question amount button animation

--- JeopardyScorer/SingleJeopardy.swift
+++ JeopardyScorer/SingleJeopardy.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import Spring
 
 class SingleJeopardy: UIViewController {
-    @IBAction func Button(sender: UIButton) {
-        NSLog("hello, world! 200 pressed.")
-    }
+    @IBOutlet var singleJeopardyUIView: UIView!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var questionAmountButton: UIButton!
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
+    let TOTAL_KEY = "total"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setTotalLabel()
+    }
+    
+    @IBAction func questionAmountButtonPressed(sender: UIButton) {
+        animateButton(sender)
+    }
+    
+    private func setTotalLabel() {
+        if let total = defaults.objectForKey(TOTAL_KEY) as? NSString {
+            totalLabel.text = String(total)
+        } else {
+            totalLabel.text = "0"
+        }
+    }
+    
+    private func animateButton(button: UIButton) {
+        spring(0.4) {
+            button.frame.origin = CGPointMake(0, 183)
+            button.titleEdgeInsets = UIEdgeInsetsMake(-350, 0, 0, 0)
+            button.frame = CGRectMake(0, 183, 380, 485)
+            self.singleJeopardyUIView.bringSubviewToFront(button)
+        }
     }
 }
 
--- Podfile
+++ Podfile
@@ -1,5 +1,6 @@
 # Uncomment this line to define a global platform for your project
-# platform :ios, '6.0'
+platform :ios, '8.0'
+use_frameworks!
 
 target 'JeopardyScorer' do
   pod 'Spring', '~> 1.0.3'
