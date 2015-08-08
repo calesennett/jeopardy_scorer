8ec0251bda5d026ec9c149bf505c5d436d8f413c
Cale Sennett <calesennett@gmail.com>
2015-08-08 11:21:45 -0400

    add view transition and actual totals

--- JeopardyScorer/Answer.swift
+++ JeopardyScorer/Answer.swift
@@ -9,10 +9,61 @@
 import UIKit
 
 class Answer: UIViewController {
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var amountButton: UIButton!
+    @IBOutlet weak var correctAnswerButton: UIButton!
+    @IBOutlet weak var incorrectAnswerButton: UIButton!
+    @IBOutlet weak var didNotAnswerButton: UIButton!
+    
+    let defaults  = NSUserDefaults.standardUserDefaults()
+    let TOTAL_KEY = "total"
+    
+    var amount : String!
+    
+    @IBAction func correctAnswerButtonDidPress(sender: UIButton) {
+        let questionButtonText = amountButton.titleLabel!.text
+        let questionAmount = questionButtonText!.substringFromIndex(advance(questionButtonText!.startIndex, 1)).toInt()
+        
+        var newTotal : Int!
+        if let previousTotal = defaults.objectForKey(TOTAL_KEY) as? Int {
+            newTotal = previousTotal + questionAmount!
+        } else {
+            newTotal = questionAmount!
+        }
+        
+        defaults.setObject(newTotal, forKey: TOTAL_KEY)
+    }
+    
+    @IBAction func incorrectAnswerButtonDidPress(sender: UIButton) {
+        let questionButtonText = amountButton.titleLabel!.text
+        let questionAmount = questionButtonText!.substringFromIndex(advance(questionButtonText!.startIndex, 1)).toInt()
+        
+        var newTotal : Int!
+        if let previousTotal = defaults.objectForKey(TOTAL_KEY) as? Int {
+            newTotal = previousTotal - questionAmount!
+        } else {
+            newTotal = -questionAmount!
+        }
+        
+        defaults.setObject(newTotal, forKey: TOTAL_KEY)
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setTotalLabel()
+        setButtonLabel()
+    }
+    
+    private func setTotalLabel() {
+        if let total = defaults.objectForKey(TOTAL_KEY) as? Int {
+            totalLabel.text = "\(total)"
+        } else {
+            totalLabel.text = "0"
+        }
+    }
+    
+    private func setButtonLabel() {
+        amountButton.setTitle(self.amount, forState: UIControlState.Normal)
     }
 }
--- JeopardyScorer/SingleJeopardy.swift
+++ JeopardyScorer/SingleJeopardy.swift
@@ -13,6 +13,9 @@ class SingleJeopardy: UIViewController {
     @IBOutlet var singleJeopardyUIView: UIView!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var questionAmountButton: UIButton!
+    @IBOutlet weak var correctButton: UIButton!
+    @IBOutlet weak var incorrectButton: UIButton!
+    @IBOutlet weak var didNotAnswerButton: UIButton!
     
     let defaults = NSUserDefaults.standardUserDefaults()
     let TOTAL_KEY = "total"
@@ -20,27 +23,65 @@ class SingleJeopardy: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setTotalLabel()
+        hideAnswerButtons()
     }
     
     @IBAction func questionAmountButtonPressed(sender: UIButton) {
         animateButton(sender)
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "questionToAnswer" {
+            let toView = segue.destinationViewController as! Answer
+            if let amount = sender!.titleLabel!!.text {
+                toView.amount = amount
+            }
+        }
+    }
+    
     private func setTotalLabel() {
-        if let total = defaults.objectForKey(TOTAL_KEY) as? NSString {
-            totalLabel.text = String(total)
+        if let total = defaults.objectForKey(TOTAL_KEY) as? Int {
+            totalLabel.text = "\(total)"
         } else {
             totalLabel.text = "0"
         }
     }
     
+    private func hideAnswerButtons() {
+        correctButton.transform = CGAffineTransformMakeTranslation(0, 300)
+        incorrectButton.transform = CGAffineTransformMakeTranslation(0, 300)
+        didNotAnswerButton.transform = CGAffineTransformMakeTranslation(0, 300)
+    }
+    
+    private func unhideAnswerButtons() {
+        springWithDelay(0.4, 0.05, {
+            self.correctButton.transform = CGAffineTransformMakeTranslation(0, 0)
+            self.singleJeopardyUIView.bringSubviewToFront(self.correctButton)
+            
+        })
+        springWithDelay(0.4, 0.10, {
+            self.incorrectButton.transform = CGAffineTransformMakeTranslation(0, 0)
+            self.singleJeopardyUIView.bringSubviewToFront(self.incorrectButton)
+            
+        })
+        springWithDelay(0.4, 0.15, {
+            self.didNotAnswerButton.transform = CGAffineTransformMakeTranslation(0, 0)
+            self.singleJeopardyUIView.bringSubviewToFront(self.didNotAnswerButton)
+            
+        })
+    }
+    
     private func animateButton(button: UIButton) {
-        spring(0.4) {
+        springWithCompletion(0.4, {
             button.frame.origin = CGPointMake(0, 183)
-            button.titleEdgeInsets = UIEdgeInsetsMake(-350, 0, 0, 0)
+            button.titleEdgeInsets = UIEdgeInsetsMake(-280, 0, 0, 0)
             button.frame = CGRectMake(0, 183, 380, 485)
             self.singleJeopardyUIView.bringSubviewToFront(button)
-        }
+            self.unhideAnswerButtons()
+            
+            }, { finished in
+                self.performSegueWithIdentifier("questionToAnswer", sender: button)
+            })
     }
 }
 
